0146d2d1bfc9e790747f66e954a2b7d2c66ca111
Edilson Borges <edilsonfb@gmail.com>
2017-03-17 00:41:32 -0300

    1st component and resolving extensions in webpack

---
+++ ex/component.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+// (it's not necessary to import react-dom here)
+
+export default function() {
+    return <h1>1st Component!</h1>
+}
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Compoment from './component'
 
-ReactDOM.render(<h1>Hi, React!</h1>, document.getElementById('app'))
+ReactDOM.render(<Compoment />, document.getElementById('app'))
