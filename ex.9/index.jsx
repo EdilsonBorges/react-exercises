cbb4cae9fdb731b47e850827821347ece5353354
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2017-03-24 08:23:36 -0300

    Applying Redux

--- ex.9/index.jsx
+++ ex.9/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import counterReducer from './counterReducer'
-import Counter from './counter'
+import Field from './field'
+import FieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-    counter: counterReducer
+    field: FieldReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Counter initialValue='Test' />
+        <Field initialValue='Test' />
     </Provider>
 , document.getElementById('app'))
